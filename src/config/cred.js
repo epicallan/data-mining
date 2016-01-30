c0dad8fa1b2184c812535d654ac11225c50dfe4c
Allan <epicallan.al@gmail.com>
2016-01-30 13:24:33 +0300

    remove credentials from remote

--- src/config/cred.js
+++
@@ -1,8 +0,0 @@
-const credentials = {
-  consumer_key: 'mEeWQoooOPsXVh8WGYaFZjU93',
-  consumer_secret: 'RtEGoK1Z1iDrGQOYqO78zn5is1YVQExwvZELSBlPhjPRtuU6KR',
-  access_token: '282545192-ub693jp5wHs0fubn05mJXqlqCFi2JKipfkGXFxs3',
-  access_token_secret: 'nwegbEIxA5p2wMfrX9CbWcFXnIJBBxWR1nt8xUZoa0qV5',
-  callback: 'http://akilihub.io/twitter/callback',
-};
-export default credentials;
